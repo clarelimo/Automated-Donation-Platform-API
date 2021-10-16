c4a4677ada1efaf19c21b679f5530f8ccdc1cf0e
clarelimo <clare.limo@student.moringaschool.com>
2021-10-16 11:45:13 +0300

    beneficiary model

---
+++ src/main/java/models/Beneficiary.java
@@ -0,0 +1,54 @@
+package models;
+
+import java.util.Objects;
+
+public class Beneficiary {
+    private String name;
+    private String testimony;
+    private String image;
+
+    public Beneficiary(String name, String testimony, String image) {
+        this.name = name;
+        this.testimony = testimony;
+        this.image = image;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getTestimony() {
+        return testimony;
+    }
+
+    public void setTestimony(String testimony) {
+        this.testimony = testimony;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Beneficiary that = (Beneficiary) o;
+        return name.equals(that.name) &&
+                testimony.equals(that.testimony) &&
+                image.equals(that.image);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, testimony, image);
+    }
+}
